13b316a044ce7a416941fc637a4bd7c1378984e2
Fox <urboifox@gmail.com>
2025-08-28 21:23:19 +0300

    ui: change loading screen colors and sizes

--- packages/core/src/ui/src/components/layout/LoadingScreen.tsx
+++ packages/core/src/ui/src/components/layout/LoadingScreen.tsx
@@ -1,10 +1,10 @@
 const LoadingScreen = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
+    <div className="flex items-center justify-center h-screen bg-background">
       <div className="relative">
-        <div className="w-20 h-20 border-t-4 border-b-4 border-green-500 rounded-full animate-spin"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-lg">
-          LENS
+        <div className="w-28 h-28 border-t-4 border-b-4 border-neutral-200 rounded-full animate-spin"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-semibold text-lg">
+          Lens.js
         </div>
       </div>
     </div>
